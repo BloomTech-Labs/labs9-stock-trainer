3fbf68e04e3c48c85060a9344888c10b9915f744
chainchompa <chainchompa@gmail.com>
2019-01-08 16:27:59 -0500

    Created sidebar for navigation

--- stock-trainer-front-end/src/App.js
+++ stock-trainer-front-end/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import SideBar from './components/SideBar';
 import TestComponent from './components/TestComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SideBar />
         <TestComponent/>
       </div>
     );
---
+++ stock-trainer-front-end/src/components/SideBar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+import "./SideBar.css";
+
+export default class SideBar extends Component {
+    state = { activeItem: 'Dashboard' }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+
+        return <div className="SideBar">
+            <p>Home > {this.state.activeItem}</p>
+            <Menu ui vertical fluid left size="massive">
+              <Menu.Item name="Dashboard" active={activeItem === "Dashboard"} onClick={this.handleItemClick} />
+              <Menu.Item name="Reports" active={activeItem === "Reports"} onClick={this.handleItemClick} />
+              <Menu.Item name="Targets" active={activeItem === "Targets"} onClick={this.handleItemClick} />
+              <Menu.Item name="Settings" active={activeItem === "Settings"} onClick={this.handleItemClick} />
+              <Menu.Item name="Billing" active={activeItem === "Billing"} onClick={this.handleItemClick} />
+            </Menu>
+          </div>;
+    }
+}
