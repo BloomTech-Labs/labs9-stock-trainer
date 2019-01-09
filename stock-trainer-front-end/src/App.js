320b12b9e6a854bec56ecebe1623933fcaeecb93
chainchompa <chainchompa@gmail.com>
2019-01-09 15:30:51 -0500

    fixed merge conflict

--- stock-trainer-front-end/src/App.js
+++ stock-trainer-front-end/src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import { StripeProvider } from 'react-stripe-elements';
-
 import './App.css';
 import SideBar from './components/SideBar';
 import TestComponent from './components/TestComponent';
-import UpgradeUser from './components/billing/UpgradeUser';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SideBar />
-        <TestComponent />
-        <StripeProvider apiKey="pk_test_12345">
-          <UpgradeUser />
-        </StripeProvider>
+        <TestComponent/>
       </div>
     );
   }
