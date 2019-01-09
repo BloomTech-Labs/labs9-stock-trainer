d6d56a7c077b2011d2858af0789235a57c60d443
chainchompa <chainchompa@gmail.com>
2019-01-09 10:09:19 -0500

    created checkout form for users to pay and upgrade to premium member, backend not started yet

--- stock-trainer-front-end/src/App.js
+++ stock-trainer-front-end/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { StripeProvider } from 'react-stripe-elements';
+
 import './App.css';
 import SideBar from './components/SideBar';
 import TestComponent from './components/TestComponent';
+import UpgradeUser from './components/billing/UpgradeUser';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SideBar />
-        <TestComponent/>
+        <TestComponent />
+        <StripeProvider apiKey="pk_test_12345">
+          <UpgradeUser />
+        </StripeProvider>
       </div>
     );
   }
