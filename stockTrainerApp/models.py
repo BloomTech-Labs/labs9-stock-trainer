893063af89ff0826d5c1be6f6ba6d19a656c4fb1
LordOrbnauticus <haywood.d.johnson@gmail.com>
2019-01-09 00:37:06 -0600

    added admin settings

--- stockTrainerApp/models.py
+++ stockTrainerApp/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
+
+
+# TODO: Create user model and other models;  django.contrib.auth.models.User allows multiple users to have accounts
 
 # Create your models here.
 
@@ -7,3 +11,6 @@ class User(models.Model):
   username = models.CharField(max_length=25)
   firstname = models.CharField(max_length=25)
   lastname = models.CharField(max_length=25)
+  created_at = models.DateTimeField(auto_now_add=True)
+  last_modified = models.DateTimeField(auto_now=True)
+
