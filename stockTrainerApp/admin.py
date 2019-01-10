27b879cbed96a49168aa14d649e25b04adc0cf9b
Jun Hyuk Kim <junhyukkeem@gmail.com>
2019-01-10 15:10:11 -0800

    setup basic usermodel with relationship with portoflio/stock

--- stockTrainerApp/admin.py
+++ stockTrainerApp/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Portfolio, Stock
 
 # Register your models here.
 
 # TODO: any other model added in .models can be registered along with user
 
-class stockTrainerAdmin(admin.ModelAdmin):
-  readonly_fields = ('created_at', 'last_modified')
 
-admin.site.register(User, stockTrainerAdmin)
+class stockTrainerAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
+
+
+admin.site.register((User, Portfolio, Stock), stockTrainerAdmin)
