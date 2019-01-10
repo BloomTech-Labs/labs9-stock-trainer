d9733990e6746f6c71b820892f5c83e6a8132cb3
Jun Hyuk Kim <junhyukkeem@gmail.com>
2019-01-10 13:28:21 -0800

    remove userinfo route from App.js for now

--- stockTrainerFrontend/src/App.js
+++ stockTrainerFrontend/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
 import "./App.css";
 import TopBar from "./components/topbar/TopBar";
 import SideBar from "./components/sidebar/SideBar";
-// import Reports from "./components/reports/Reports";
-import UserInfo from "./components/userinfo/UserInfo";
+import Reports from "./components/reports/Reports";
+// import UserInfo from "./components/userinfo/UserInfo";
 
 class App extends Component {
   constructor(props) {
@@ -23,8 +23,7 @@ class App extends Component {
         <TopBar currentUser={currentUser} signedInState={signedIn} />
         <div className="lowerPageLayout">
           <SideBar />
-          {/* <Reports /> */}
-          <Route path="/user/:id" component={UserInfo} />
+          <Reports />
         </div>
       </div>
     );
