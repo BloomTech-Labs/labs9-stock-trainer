4a478f076a12c03068a815eb6ac7a1ee47eac65a
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-01-14 15:45:32 -0500

    added auth0 bolierplate

---
+++ stockTrainerFrontend/src/Auth.js
@@ -0,0 +1,65 @@
+import auth0 from "auth0-js";
+
+class Auth {
+  constructor() {
+    this.auth0 = new auth0.WebAuth({
+      // the following three lines MUST be updated
+      domain: "stock-trainer.auth0.com",
+      audience: "https://stock-trainer.auth0.com/userinfo",
+      clientID: "90QRusJ6F6LjsgmUA97iEc2rXfDANPqa",
+      redirectUri: "https://stock-trainer.netlify.com/callback",
+      responseType: "id_token",
+      scope: "openid profile"
+    });
+
+    this.getProfile = this.getProfile.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.signIn = this.signIn.bind(this);
+    this.signOut = this.signOut.bind(this);
+  }
+
+  getProfile() {
+    return this.profile;
+  }
+
+  getIdToken() {
+    return this.idToken;
+  }
+
+  isAuthenticated() {
+    return new Date().getTime() < this.expiresAt;
+  }
+
+  signIn() {
+    this.auth0.authorize();
+  }
+
+  handleAuthentication() {
+    return new Promise((resolve, reject) => {
+      // eslint-disable-next-line consistent-return
+      this.auth0.parseHash((err, authResult) => {
+        if (err) return reject(err);
+        if (!authResult || !authResult.idToken) {
+          return reject(err);
+        }
+        this.idToken = authResult.idToken;
+        this.profile = authResult.idTokenPayload;
+        // set the time that the id token will expire at
+        this.expiresAt = authResult.idTokenPayload.exp * 1000;
+        resolve();
+      });
+    });
+  }
+
+  signOut() {
+    // clear id token, profile, and expiration
+    this.idToken = null;
+    this.profile = null;
+    this.expiresAt = null;
+  }
+}
+
+const auth0Client = new Auth();
+
+export default auth0Client;
