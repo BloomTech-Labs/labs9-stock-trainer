5269dd2284fcb392a940713d68ea735e50947907
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-01-14 12:40:29 -0500

    added a remote user middleware

--- stockTrainerBackEnd/settings.py
+++ stockTrainerBackEnd/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware'
