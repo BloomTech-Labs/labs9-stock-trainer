42661808a5fba0f83b84955211e98813cf7b7a39
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-01-16 13:54:06 -0500

    took out a console.log

--- stockTrainerFrontend/src/Auth/Callback.js
+++ stockTrainerFrontend/src/Auth/Callback.js
@@ -3,7 +3,6 @@ import { withRouter } from "react-router-dom";
 
 const Callback = props => {
   const { auth } = props;
-  console.log(auth);
   auth.handleAuthentication().then(() => {
     props.history.push("/dashboard");
     props.signinchange();
