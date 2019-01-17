685ff7558557a9fd7af83eda35334da42c597f00
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-01-17 16:53:07 -0500

    added axios call to auth0 backend

--- stockTrainerFrontend/src/App.js
+++ stockTrainerFrontend/src/App.js
@@ -55,18 +55,22 @@ class App extends Component {
     auth.signIn();
   };
 
-  retrieveUser = props => {
+  retrieveUser = () => {
+    const { auth } = this.props;
     axios
       .request({
         method: "get", // I think this should be a get?
-        baseURL: `${process.env.REACT_APP_BACKEND_URL}User/`, // so basically replace stock/ with whatever you need to hit. tthe first / is in the env file
+        baseURL: `${process.env.REACT_APP_BACKEND_URL}current_user/`, // so basically replace stock/ with whatever you need to hit. tthe first / is in the env file
         headers: {
-          Authorization: `Bearer ${props.auth.accessToken}` // I'm pretty sure this is right, make sure it is before using it
+          Authorization: `Bearer ${auth.accessToken}` // I'm pretty sure this is right, make sure it is before using it
         }
       })
       .then(res => {
         // your response is going to see the res here, including http code and whatever. res.data normally has whatever is given back to you
-        res.data();
+        // res.data();
+        this.switchSignInState({
+          name: res.data.UserInfo
+        });
       })
       .catch(err => {
         // eslint-disable-next-line no-console
