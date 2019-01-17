13f786885500ccafb2a5003a1c398ef25ebec0aa
LaikaFusion <newrad4@gmail.com>
2019-01-17 16:06:05 -0500

    tweaks to restore some of the alignment

--- stockTrainerFrontend/src/components/stock/Stock.jsx
+++ stockTrainerFrontend/src/components/stock/Stock.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { Header, Icon } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
 import "./Stock.css";
 
 const Stock = props => {
   const { name } = props;
   return (
     <div className="stock">
-      <Header className="stockName" as="h1">
-        {name}
-      </Header>
+      <h1 className="stockName">{name}</h1>
       <Icon className="favoriteIcon" name="star outline" size="big" />
       <div className="leftColumnInfobox">
         <div className="upperRowInfobox">Price: $148.40</div>
