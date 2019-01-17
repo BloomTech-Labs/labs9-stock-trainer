c06a1593eb0af1892c60fb744b2040d054cae7a9
LordOrbnauticus <haywood.d.johnson@gmail.com>
2019-01-16 19:10:27 -0600

    fixed. ready to merge with new routes

--- stockTrainerBackEnd/settings.py
+++ stockTrainerBackEnd/settings.py
@@ -112,7 +112,7 @@ DATABASES['default'] = dj_database_url.config(
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
