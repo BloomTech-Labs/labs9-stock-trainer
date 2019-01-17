92643e858a2426025f450a69ab21c87ec1c1861a
LaikaFusion <newrad4@gmail.com>
2019-01-17 16:58:42 -0500

    added 404 page

--- stockTrainerFrontend/src/components/nomatch/NoMatch.jsx
+++ stockTrainerFrontend/src/components/nomatch/NoMatch.jsx
@@ -3,7 +3,14 @@ import { Segment } from "semantic-ui-react";
 import "./NoMatch.css";
 
 function NoMatch() {
-  return <Segment>404</Segment>;
+  return (
+    <Segment className="warning">
+      <div className="four">404</div>
+      <div className="sorryMessage">
+        The page you are looking for can not be found.
+      </div>
+    </Segment>
+  );
 }
 
 export default NoMatch;
