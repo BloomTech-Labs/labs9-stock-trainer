4aea62bc1332f28cf0cb5560bb40b8231cc191fe
LaikaFusion <newrad4@gmail.com>
2019-01-22 17:51:06 -0500

    really janky auto scroll added

--- stockTrainerFrontend/src/components/dashboard/Dashboard.jsx
+++ stockTrainerFrontend/src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { Segment, Input } from "semantic-ui-react";
+import { Segment } from "semantic-ui-react";
 import "./Dashboard.css";
 import Indicators from "./Indicators";
-import Stock from "../stock/Stock";
 import Favorites from "../favorites/Favorites";
-
-const dummyData = [{ name: "Apple" }, { name: "Amazon" }];
 
 const Dashboard = props => {
   const { favorites } = props;
@@ -15,15 +12,10 @@ const Dashboard = props => {
         <Indicators />
       </Segment>
       <Segment className="userStocks">
-        <Favorites favorites={favorites} />
+        <Favorites title="Favorites" favorites={favorites} />
       </Segment>
       <Segment className="leftColumn">
-        <Input placeholder="Search..." className="stockSearch" />
-        {dummyData.map(stock => (
-          <div className="stockContainer" key={stock.name}>
-            <Stock name={stock.name} />
-          </div>
-        ))}
+        <Favorites title="Stock Ticker" favorites={favorites} />
       </Segment>
     </div>
   );
--- stockTrainerFrontend/src/components/favorites/Favorites.jsx
+++ stockTrainerFrontend/src/components/favorites/Favorites.jsx
@@ -1,55 +1,93 @@
 import React from "react";
 import { Input, Segment, Header, List } from "semantic-ui-react";
 import Stock from "../stock/Stock";
+
 import "./Favorites.css";
 
-const Favorites = props => {
-  // let { favorites } = props;
+class Favorites extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const favorites = [
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" },
-    { name: "testTEST" }
-  ];
+    this.state = {
+      items: [
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" },
+        { name: "testTEST" }
+      ]
+    };
+  }
 
-  return (
-    <div className="favoritesHolder">
-      <Header attached="top">
-        <h2>Favorites</h2>
-        <Input
-          placeholder="Search for stock"
-          value=""
-          onChange=""
-          fluid
-          action={{
-            content: "search",
-            onClick: () => {}
-          }}
-        />
-      </Header>
+  handleScroll = e => {
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    if (bottom) {
+      this.setState({
+        items: this.state.items.concat([
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" },
+          { name: "testTEST" }
+        ])
+      })
+     
+    }
+  };
 
-      <Segment className="favoritesDisplay" attached>
-        <List className="helpSearch" divided>
-          {favorites.map((e, i) => (
-            <List.Item key={i} className="favoritesItem">
-              <Stock name={e.name} />
+  render() {
+    const { title } = this.props;
+    const { items } = this.state;
+    return (
+      <div className="favoritesHolder">
+        <Header attached="top">
+          <h2>{title}</h2>
+          <Input
+            placeholder="Search for stock"
+            value=""
+            onChange=""
+            fluid
+            action={{
+              content: "search",
+              onClick: () => {}
+            }}
+          />
+        </Header>
+
+        <Segment className="favoritesDisplay" onScroll={this.handleScroll} attached>
+          <List className="helpSearch" divided >
+            {items.map((e, i) => (
+              <List.Item key={i} className="favoritesItem">
+                <Stock name={e.name} />
+              </List.Item>
+            ))}
+            <List.Item>
+              <h3>Loadering...</h3>
             </List.Item>
-          ))}
-        </List>
-      </Segment>
-    </div>
-  );
-};
+          </List>
+        </Segment>
+      </div>
+    );
+  }
+}
 
 export default Favorites;
