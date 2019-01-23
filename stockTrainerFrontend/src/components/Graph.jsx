4fd706e9bcf25b174d96e8cdb4538fde5d329b37
Jun Hyuk Kim <junhyukkeem@gmail.com>
2019-01-23 13:26:17 -0800

    implement data conversion for date and close

--- stockTrainerFrontend/src/components/Graph.jsx
+++ stockTrainerFrontend/src/components/Graph.jsx
@@ -4,25 +4,44 @@ import { VictoryChart, VictoryLine } from "victory";
 import "./Graph.css";
 
 // data should consist of an array of objects
-const data = [
-  { x: 1, y: 13000 },
-  { x: 2, y: 16500 },
-  { x: 3, y: 14250 },
-  { x: 4, y: 19000 }
+const testData = [
+  { date: "2015-12-07", close: "118.8" },
+  { date: "2015-12-08", close: "121.8" },
+  { date: "2015-12-09", close: "141.8" },
+  { date: "2015-12-10", close: "118.8" }
 ];
 
 // currently throws some warnings about non-boolean attributes receiving boolean values, will need to fix
 class Graph extends React.Component {
   constructor(props) {
     super(props);
-    console.log(`constructor`);
+    this.state = { data: testData };
   }
 
+  componentDidMount() {
+    this.convertData();
+  }
+
+  convertData = () => {
+    const { data } = this.state;
+    const dataCopy = JSON.parse(JSON.stringify(data));
+    const newData = dataCopy.map(dataPoint => {
+      return {
+        date: new Date(dataPoint.date),
+        close: Number(dataPoint.close)
+      };
+    });
+    this.setState({
+      data: newData
+    });
+  };
+
   render() {
-    console.log(data);
+    const { data } = this.state;
+    // console.log(data);
     return (
       <Segment className="graph">
-        <VictoryChart>
+        <VictoryChart scale={{ x: "time" }}>
           <VictoryLine
             style={{
               data: { stroke: "#0984e3" }, // color of the stroke
@@ -30,7 +49,7 @@ class Graph extends React.Component {
             }}
             data={data}
             x="date"
-            y={data.price}
+            y="close"
             animate={{
               // animation can be implemented
               duration: 2000,
