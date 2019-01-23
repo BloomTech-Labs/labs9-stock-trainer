8daac53488c8eb3e76598348c36d9da659d122d1
LaikaFusion <newrad4@gmail.com>
2019-01-23 16:04:55 -0500

    styled search bar with auto suggest

--- stockTrainerFrontend/src/components/reports/Reports.jsx
+++ stockTrainerFrontend/src/components/reports/Reports.jsx
@@ -1,10 +1,84 @@
 import React from "react";
 import { Button, Input, Tab, Segment } from "semantic-ui-react";
 import "./Reports.css";
+import Autosuggest from "react-autosuggest";
+import data from "../../util/test.json";
+
 import Stock from "../stock/Stock";
 
 const today = new Date().toISOString().substr(0, 10);
+const theme = {
+  container: {
+    height: "90%",
+    fontSize: "24px"
+  },
+  suggestionsContainerOpen: {
+    position: "absolute",
+    backgroundColor: "white",
+    zIndex: "4",
+    overflow: "auto",
+    boxShadow: "0 1px 2px 0 rgba(34,36,38,.15)",
+    border: "1px solid rgba(34,36,38,.15)",
+    overflowY: "auto",
+    maxHeight: "600%",
+    left: "0",
+    right: "0"
+    // width: "calc(60% - 6rem)"
+  },
+  suggestionsList: {
+    zIndex: "4",
+    paddingLeft: "0",
+    marginTop: ".6rem"
+  },
+  suggestion: {
+    cursor: "pointer",
+    padding: "10px 20px",
+    listStyleType: "none",
+    lineHeight: "1.2em",
+    borderTop: "1px solid rgba(34,36,38,.15)"
+  },
+  suggestionHighlighted: {
+    backgroundColor: "#ddd"
+  },
+  input: {
+    maxWidth: "100%",
+    width: "100%",
+    border: "1px solid rgba(34,36,38,.15)",
+    color: "rgba(0,0,0,.87)",
+    height: "100%",
+    margin: "0"
+  },
+  suggestionFirst: {
+    border: "0"
+  }
+};
 
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0
+    ? []
+    : data.filter(
+        lang =>
+          lang.name.toLowerCase().slice(0, inputLength) === inputValue ||
+          lang.symbol.toLowerCase().slice(0, inputLength) === inputValue
+      );
+};
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+// this will be the main function to allow suggestion -> symbol link
+const getSuggestionValue = suggestion => suggestion.symbol;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div>
+    {suggestion.name} - <strong>{suggestion.symbol}</strong>
+  </div>
+);
 const placeholderPane = (
   <Segment className="chartArea">
     <Tab.Pane>Placeholder image</Tab.Pane>
@@ -37,18 +111,63 @@ export default class Reports extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { value: "", suggestions: [] };
   }
 
   componentDidMount = () => {};
 
   findData = () => {};
 
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  searchStock = () => {
+    const { retrieveStock } = this.props;
+    const { value } = this.state;
+    if (value === "") {
+      return;
+    }
+    retrieveStock(value.toUpperCase());
+  };
+
   render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Look for a company!",
+      value,
+      onChange: this.onChange
+    };
     return (
       <Segment className="reportsContainer">
         <Stock big symbol="test" name="Stock name here" />
-        <Input className="reportsSearch" placeholder="Search..." />
+        <div className="reportsSearch">
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+            theme={theme}
+          />
+        </div>
+
         <div id="from" className="dateSelect">
           <h3>From:</h3>
           <Input
