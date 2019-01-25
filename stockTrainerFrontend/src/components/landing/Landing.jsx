877e9d19d989bc990bc40c9019c5b8882fc844c3
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-01-25 13:29:03 -0500

    working on image

--- stockTrainerFrontend/src/components/landing/Landing.jsx
+++ stockTrainerFrontend/src/components/landing/Landing.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { Container, Divider, Button, Header } from "semantic-ui-react";
+import { Container, Divider, Button, Header, Image } from "semantic-ui-react";
 
 const Landing = () => (
   <Container fluid textAlign="center">
     <div>
-      <img src=".../images/stock.jpg" alt="stock" />
+      <Image src=".../images/stock.jpg" alt="stock" />
     </div>
     <Divider />
     <Container text>
