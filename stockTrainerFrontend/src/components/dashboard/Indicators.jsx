427b4cdad90824703fbb625c0467501228f39b57
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-01-29 13:32:29 -0500

    added 3 indicators

--- stockTrainerFrontend/src/components/dashboard/Indicators.jsx
+++ stockTrainerFrontend/src/components/dashboard/Indicators.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Carousel from "re-carousel";
 
@@ -15,10 +16,27 @@ export default function Indicators() {
         </p>
       </div>
       <div>
-        <h3>Indicator 2</h3>
+        <h3>Volume Weighted Average Price</h3>
+        <p>
+          Volume Weighted Average Price(VWAP) is primarily used for pension
+          plans. You can calculate it by adding all the dollars for every
+          transaction and divide that by the total shares of the day. Typically
+          used by instutional investors to buy and sell large orders and not
+          disturb the market with the large number of orders.
+        </p>
       </div>
       <div>
-        <h3>Indicator 3</h3>
+        <h3>Moving Average</h3>
+        <p>
+          Moving averages are the most used and popular tool by investors to
+          measure a stocks momentum. There are 3 types of Moving Averages:
+          Simple Moving Average, Weighted Moving Average, and Exponential Moving
+          Average. Simple Moving average was used before the rise of computers
+          for it's simplicity to calculate. Weighted Moving Average is heavier
+          and more complicated to calculate using the current data compared to
+          past data. Exponential is the same as Weighted but the decrease
+          between prices is not consistent.
+        </p>
       </div>
     </Carousel>
   );
