9e639c97a326087fd42ec62e43b6fea55dece8e6
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-02-05 16:42:39 -0500

    added deffinition change to sar

--- stockTrainerFrontend/src/components/help/SAR.jsx
+++ stockTrainerFrontend/src/components/help/SAR.jsx
@@ -5,30 +5,31 @@ import { Container, Header } from "semantic-ui-react";
 import SAR from "./images/SAR.png";
 import "./Opening.css";
 
-const StockAppreciation = () => (
+const ParabolicSar = () => (
   <Container text>
-    <Header as="h1">Stock Appreciation Right (SAR)</Header>
+    <Header as="h1">Parabolic SAR</Header>
     <p>
-      Stock Appreciation Right is, as stated in{" "}
-      <a target="_blank" href="https://www.investopedia.com/terms/s/sar.asp">
+      Parabolic SAR attemps, as stated in{" "}
+      <a
+        target="_blank"
+        href="https://www.investopedia.com/trading/introduction-to-parabolic-sar/"
+      >
         Investopedia
       </a>
-      "a bonus given to employees that is equal to the appreciation of a company
-      stock over an extablished time period." They are similar to employee stock
-      options. Some differences are employees don't have to pay the exercise
-      price when the company stock prices rise and employees can recieve
-      proceeds from stock price increases without buying anything. Employees
-      recieve the sum of the increase in stock or cash.
+      "to give traders an edge by highlighting the direction an assest is
+      moving, as well as providing entry and exit points." It is a techinical
+      indicator that determines the price direction and provides attention when
+      the price direction is changing.
     </p>
     <p>
-      There are plenty of benefits of SAR's with the greatest being flexibility.
-      They can be created differently for each employee. But because of this
-      some of the flaws are there are numerous choices to make. SAR's offer the
-      right to cash. They are almost always paid in cash.
+      It is indicated on a chart by a series of darts either above or below the
+      price line. If the darts are below the sign it means the price direction
+      is moving upwards. And if the dots are above the line that means the price
+      is moving downwards.
     </p>
     <img src={SAR} alt="Apple Graph" className="open" />
     {/* Image was screenshotted using stock trainers graphs */}
   </Container>
 );
 
-export default StockAppreciation;
+export default ParabolicSar;
